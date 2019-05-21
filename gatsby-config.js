ab569cfe82bc75433c577485d9d6e5f40eb19bca
Khoa Bui <thomasbui93@gmail.com>
2019-05-22 00:11:30 +0100

    add images source to the configuration

--- gatsby-config.js
+++ gatsby-config.js
@@ -22,6 +22,13 @@ module.exports = {
         name: `blog`,
       },
     },
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `img`,
+        path: `${__dirname}/src/images/`
+      }
+    },
     `gatsby-transformer-sharp`,
     `gatsby-plugin-sharp`,
     {
